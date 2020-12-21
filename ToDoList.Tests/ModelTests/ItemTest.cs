35230cf0519c044430a7b2516df0f7cee74caa64
Kaila Sprague McRae <kaila.sprague@icloud.com>
2020-12-21 06:41:17 -0800

    update some todolist tests to set up database

--- ToDoList.Tests/ModelTests/ItemTest.cs
+++ ToDoList.Tests/ModelTests/ItemTest.cs
@@ -38,7 +38,20 @@ namespace ToDoList.Tests
       // Assert
       Assert.AreEqual(firstItem, secondItem);
     }
+    [TestMethod]
+    public void Save_SavesToDatabase_ItemList()
+    {
+    //Arrange
+      Item testItem = new Item("Mow the lawn");
 
+      //Act
+      testItem.Save();
+      List<Item> result = Item.GetAll();
+      List<Item> testList = new List<Item>{testItem};
+
+      //Assert
+      CollectionAssert.AreEqual(testList, result);
+    }
 
     // [TestMethod]
     // public void ItemConstructor_CreatesInstanceOfItem_Item()
@@ -81,22 +94,24 @@ namespace ToDoList.Tests
     //   // Assert
     //   CollectionAssert.AreEqual(newList, result);
     // }
-    // [TestMethod]
-    // public void GetAll_ReturnsItems_ItemList()
-    // {
-    //   //Arrange
-    //   string description01 = "Walk the dog";
-    //   string description02 = "Wash the dishes";
-    //   Item newItem1 = new Item(description01);
-    //   Item newItem2 = new Item(description02);
-    //   List<Item> newList = new List<Item> { newItem1, newItem2 };
+    [TestMethod]
+    public void GetAll_ReturnsItems_ItemList()
+    {
+      //Arrange
+      string description01 = "Walk the dog";
+      string description02 = "Wash the dishes";
+      Item newItem1 = new Item(description01);
+      newItem1.Save();
+      Item newItem2 = new Item(description02);
+      newItem2.Save();
+      List<Item> newList = new List<Item> { newItem1, newItem2 };
 
-    //   //Act
-    //   List<Item> result = Item.GetAll();
+      //Act
+      List<Item> result = Item.GetAll();
 
-    //   //Assert
-    //   CollectionAssert.AreEqual(newList, result);
-    // }
+      //Assert
+      CollectionAssert.AreEqual(newList, result);
+    }
     // [TestMethod]
     // public void GetId_ItemsInstantiateWithAnIdAndGetterReturns_Int()
     // {
@@ -108,18 +123,19 @@ namespace ToDoList.Tests
     //   //Assert
     //   Assert.AreEqual(1,result);
     // }
-    // [TestMethod]
-    // public void Find_ReturnsCorrectItem_Item()
-    // {
-    //   //Arrage
-    //   string description01 = "Walk the dog.";
-    //   string description02 = "Wash the dished.";
-    //   Item newItem1 = new Item(description01);
-    //   Item newItem2 = new Item(description02);
-    //   //Act
-    //   Item result = Item.Find(2);
-    //   //Assert
-    //   Assert.AreEqual(newItem2, result);
-    // }
+    [TestMethod]
+    public void Find_ReturnsCorrectItemFromDatabase_Item()
+    {
+     //Arrange
+      Item newItem = new Item("Mow the lawn");
+      newItem.Save();
+      Item newItem2 = new Item("Wash dishes");
+      newItem2.Save();
+
+      //Act
+      Item foundItem = Item.Find(newItem.Id);
+      //Assert
+      Assert.AreEqual(newItem, foundItem);
+    }
   }
 }
